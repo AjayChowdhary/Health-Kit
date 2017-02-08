c25f7222aa0e3b3a688e94fc3503174b7a096675
Ajay Chowdhary <archh1096@gmail.com>
2017-02-08 17:14:45 +0530

    Adding App and php code

---
+++ mystyle.php
@@ -0,0 +1,97 @@
+
+body{background-color:#45a049;}
+
+
+input[type=text],select{ width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+ }
+input[type=password]{ width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+ }
+input[type=submit] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+#a {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+     width:50%;
+        height:175%;
+
+ 
+ margin: 0;
+
+
+
+}
+#b{
+background-color: black;
+width: 20%;
+padding: 20px;
+float:left;
+}
+label{
+float:left;
+}
+input[type=textarea]{
+
+ border-radius: 5px;
+ width: 100%;
+ height: 100px;
+ padding: 20px;
+ text: black;
+}
+#b{
+background-color: black;
+width: 20%;
+padding: 20px;
+float:left;
+}
+label{
+float:left;
+}
+input[type=textarea]{
+
+ border-radius: 5px;
+ width: 100%;
+ height: 100px;
+ padding: 20px;
+ text: black;
+ box-sizing: border-box;
+ padding:14px 30px;
+
+}
+
+#b,div {
+margin-left:20%;
+margin-top:0;
+
+padding:3px 15px;
+height:500px;
+
+
+
+}
+
+
---
+++ popoopopop/app/src/main/java/com/example/ajaychowdhary/popoopopop/JsonParser.java
@@ -0,0 +1,9 @@
+package com.example.ajaychowdhary.popoopopop;
+
+/**
+ * Created by AJAY CHOWDHARY on 07-02-2017.
+ */
+
+public class JsonParser {
+
+}
